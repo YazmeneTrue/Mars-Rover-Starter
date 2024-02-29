be77c01bccbc6c16a342b3f163f7fe714c68bcd1
Yazzy Boo <yazzyboo@Chazzs-MacBook-Pro.local>
2024-02-29 12:48:58 -0500

    done rendering

--- rover.js
+++ rover.js
@@ -1,41 +1,45 @@
 class Rover {
    constructor(position, mode = 'NORMAL', generatorWatts = 110) {
-      this.position = position;
-      this.mode = mode;
-      this.generatorWatts = generatorWatts;
+     this.position = position;
+     this.mode = mode;
+     this.generatorWatts = generatorWatts;
    }
  
    receiveMessage(message) {
-      const results = message.commands.map(command => {
-         if (command.commandType === 'STATUS_CHECK') {
-            return {
-               completed: true,
-               roverStatus: {
-                  position: this.position,
-                  mode: this.mode,
-                  generatorWatts: this.generatorWatts
-               }
-            };
-         } else if (command.commandType === 'MODE_CHANGE') {
-            this.mode = command.value;
-            return { completed: true };
-         } else if (command.commandType === 'MOVE') {
-            if (this.mode === 'LOW_POWER') {
-               return { completed: false };
-            } else {
-               this.position = command.value;
-               return { completed: true };
-            }
+     const results = [];
+     
+     for (let i = 0; i < message.commands.length; i++) {
+       const command = message.commands[i];
+       
+       if (command.commandType === 'STATUS_CHECK') {
+         results.push({
+           completed: true,
+           roverStatus: {
+             position: this.position,
+             mode: this.mode,
+             generatorWatts: this.generatorWatts
+           }
+         });
+       } else if (command.commandType === 'MODE_CHANGE') {
+         this.mode = command.value;
+         results.push({ completed: true });
+       } else if (command.commandType === 'MOVE') {
+         if (this.mode === 'LOW_POWER') {
+           results.push({ completed: false });
          } else {
-            return { completed: false };
+           this.position = command.value;
+           results.push({ completed: true });
          }
-      });
+       } else {
+         results.push({ completed: false });
+       }
+     }
  
-      return {
-         message: message.name,
-         results: results
-      };
+     return {
+       message: message.name,
+       results: results
+     };
    }
-}
+ }  
 
 module.exports = Rover;
